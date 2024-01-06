4055cb32be9074cab6496f211cdaa10263eab293
uzeyir-yariz <99393019+uzeyir-yariz@users.noreply.github.com>
2024-01-06 22:37:10 +0300

    optimize and add a somting

--- derleme.ino
+++ derleme.ino
@@ -1,122 +1,88 @@
-/*
-  30'dan başlıyor
-  45'de bitiyor
+/* 
+
+  |--------------------------|====--            --====|
+  |copy right by uzeyir-yariz|==-- code with live --==|
+  |--------------------------|====--            --====|
+  | > github    : https://github.com/uzeyir-yariz
+  | > linkedin  : https://www.linkedin.com/in/üzeyir-yariz-aa2722259/
+  | > web site  : https://uzeyir-yariz.com.tr
+  | > blog site : https://oldeblog.com.tr
+  | > instagram : https://www.instagram.com/uzeyiryariz/
+
 */
 
+// 30'dan başlıyor 45'de bitiyor 
 int randNumber;
+int bekleme_suresi = 1000; 
+
+// Dizi değişkenleri
+int Y[] = {30, 31, 32, 33, 38, 39, 40, 41};  // Yeşil pinler
+int K[] = {34, 35, 36, 37, 42, 43, 44, 45};  // Kırmızı pinler
+
+// şablon
+void yakim(int index, int modulo) {
+  if (randNumber % modulo == 0) {
+    digitalWrite(Y[index], 1);  // yeşil
+    digitalWrite(K[index], 0);   // kırmızı
+  } else {
+    digitalWrite(Y[index], 0);   // yeşil
+    digitalWrite(K[index], 1);  // kırmızı
+  }
+}
 
 void setup() {
   Serial.begin(9600);
 
-  for(int i = 30; i <= 45; i++) {
-    pinMode(i, OUTPUT);
+  // Pin modlarını ayarla
+  for (int i = 0; i < 8; ++i) {
+    pinMode(Y[i], OUTPUT);
+    pinMode(K[i], OUTPUT);
   }
 
-  pinMode(resetput, INPUT);
+  open();
 }
 
+// ana döngü 
 void loop() {
+  // make_uzy();
+
   all_led_on();
 
-  delay(1000);
+  delay(bekleme_suresi);
 }
 
 // rastgele sayı oluşturun 
-void random_num_creat(){randNumber = random(1000, 9999); Serial.print(randNumber);}
-
-// ayrım 
-void yakim_two(){
-  if(randNumber % 2 == 0){
-    digitalWrite(30, 1); // yeşil
-    digitalWrite(34, 0); // kırmızı 
-  } else{
-    digitalWrite(30, 0); // yeşil
-    digitalWrite(34, 1); // kırmızı
-  }}
-
-void yakim_three() {
-  if(randNumber % 3 == 0){
-    digitalWrite(31, 1); // yeşil
-    digitalWrite(35, 0); // kırmızı
-  } else{
-    digitalWrite(31, 0); // yeşil
-    digitalWrite(35, 1); // kırmızı
-  }}
-
-void yakim_four() {
-  if(randNumber % 4 == 0){
-    digitalWrite(32, 1); // yeşil
-    digitalWrite(36, 0); // kırmızı
-  } else{
-    digitalWrite(32, 0); // yeşil
-    digitalWrite(36, 1); // kırmızı
-  }}
-
-void yakim_five() {
-  if(randNumber % 5 == 0){
-    digitalWrite(33, 1); // yeşil
-    digitalWrite(37, 0); // kırmızı
-  } else{
-    digitalWrite(33, 0); // yeşil
-    digitalWrite(37, 1); // kırmızı
-  }}
-
-void yakim_six() {
-  if(randNumber % 6 == 0){
-    digitalWrite(38, 1); // yeşil
-    digitalWrite(42, 0); // kırmızı
-  } else{
-    digitalWrite(38, 0); // yeşil
-    digitalWrite(42, 1); // kırmızı
-  }}
-
-void yakim_seven() {
-  if(randNumber % 7 == 0){
-    digitalWrite(39, 0); // yeşil
-    digitalWrite(43, 1); // kırmızı
-  } else{
-    digitalWrite(39, 1); // yeşil
-    digitalWrite(43, 0); // kırmızı
-  }}
-void yakim_eight() {
-  if(randNumber % 8 == 0){
-    digitalWrite(40, 0); // yeşil
-    digitalWrite(44, 1); // kırmızı
-  } else{
-    digitalWrite(40, 1); // yeşil
-    digitalWrite(44, 0); // kırmızı
-  }}
-
-void yakim_nine() {
-  if(randNumber % 9 == 0){
-    digitalWrite(41, 1); // yeşil
-    digitalWrite(45, 0); // kırmızı
-  } else{
-    digitalWrite(41, 0); // yeşil
-    digitalWrite(45, 1); // kırmızı
-  }}
-
-// ayrım 
+void random_num_creat(){randNumber = random(1000, 9999); Serial.println(randNumber);}
 
 // ışıkları açın / kapatın 
-void light_on(){for(int i = 30; i <= 45; i++){digitalWrite(i, 1);}}
-void light_of(){for(int i = 30; i <= 45; i++){digitalWrite(i, 0);}}
+void light_on(){for(int i = 30; i <= 45; i++){digitalWrite(i, 1); delay(50);}}
+void light_of(){for(int i = 30; i <= 45; i++){digitalWrite(i, 0); delay(50);}}
+
+// açılış
+void open(){
+  light_on();
+  delay(750);
+  light_of();
+  delay(500);
+  led_on(); delay(200);
+  led_of(); delay(200);
+  led_on(); delay(200);
+  led_on(); delay(200);
+  led_of(); delay(200);}
 
 // tüm ledleri aktif eder
 void all_led_on(){
   random_num_creat();
-
-  yakim_two();
-  yakim_three();
-  yakim_four();
-  yakim_five();
-  yakim_six();
-  yakim_seven();
-  yakim_eight();
-  yakim_nine();}
-
-
-
+    
+  yakim(0, 2);
+  yakim(1, 3);
+  yakim(2, 4);
+  yakim(3, 5);
+  yakim(4, 6);
+  yakim(5, 7);
+  yakim(6, 8);
+  yakim(7, 9);
+  }
 // özel ayar 
 void ileri_yakim(){
   for(int i = 30; i <= 45; i++) {
@@ -130,3 +96,64 @@ void ileri_yakim(){
     digitalWrite(i, 0);
     delay(500);
   }}
+
+// led ayarları
+void led_on(){
+    for(int i = 30; i <= 45; i++) {
+    digitalWrite(i, 1);
+  }}
+void led_of(){
+    for(int i = 30; i <= 45; i++) {
+    digitalWrite(i, 0);
+  }}
+// harfler
+void make_u(){
+  digitalWrite(Y[0], 1); 
+  digitalWrite(Y[1], 1); 
+  digitalWrite(Y[2], 1); 
+  digitalWrite(K[3], 1); 
+  digitalWrite(Y[7], 1); 
+  digitalWrite(K[6], 1); 
+  digitalWrite(K[5], 1); 
+  digitalWrite(K[4], 1); delay(bekleme_suresi); led_of();}
+void make_z(){
+  digitalWrite(Y[0], 1); 
+  digitalWrite(K[0], 1); 
+  digitalWrite(Y[4], 1); 
+  digitalWrite(K[4], 1); 
+  digitalWrite(Y[5], 1); 
+  digitalWrite(K[2], 1); 
+  digitalWrite(Y[3], 1); 
+  digitalWrite(K[3], 1); 
+  digitalWrite(Y[7], 1); 
+  digitalWrite(K[7], 1); delay(bekleme_suresi); led_of();}
+void make_y(){  
+  digitalWrite(Y[0], 1); 
+  digitalWrite(Y[1], 1); 
+  digitalWrite(K[1], 1); 
+  digitalWrite(Y[5], 1); 
+  digitalWrite(K[5], 1); 
+  digitalWrite(K[4], 1);  
+  digitalWrite(K[6], 1);  
+  digitalWrite(K[7], 1);  
+  digitalWrite(Y[7], 1);  
+  digitalWrite(K[3], 1);  
+  digitalWrite(Y[3], 1); delay(bekleme_suresi); led_of();}
+// harfleri sırayla yak
+void make_uzy(){  
+  make_u();
+  make_z();
+  make_y();}
+
+/* 
+
+  |--------------------------|====--            --====|
+  |copy right by uzeyir-yariz|==-- code with live --==|
+  |--------------------------|====--            --====|
+  | > github    : https://github.com/uzeyir-yariz
+  | > linkedin  : https://www.linkedin.com/in/üzeyir-yariz-aa2722259/
+  | > web site  : https://uzeyir-yariz.com.tr
+  | > blog site : https://oldeblog.com.tr
+  | > instagram : https://www.instagram.com/uzeyiryariz/
+
+*/
